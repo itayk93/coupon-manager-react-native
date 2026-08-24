5173dc91e050a1068d90f8fc119e832b69cb4ca9
itayk93 <itayk93@gmail.com>
2026-08-24 07:16:48 +0300

    Prevent native screen codegen crash on web

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,14 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+const defaultResolveRequest = config.resolver.resolveRequest;
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === "web" && moduleName === "react-native-screens") {
+    return { type: "sourceFile", filePath: `${__dirname}/src/web/react-native-screens.js` };
+  }
+  return defaultResolveRequest
+    ? defaultResolveRequest(context, moduleName, platform)
+    : context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = config;
